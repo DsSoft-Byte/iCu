9cd28ac64da5372dcdbc9c0bd018db3a88625f3e
DsSoft-Byte <djarocki7@gmail.com>
2022-05-30 20:37:28 +0200

    Add Compiled Programm files

---
+++ iCures/idact.bat
@@ -0,0 +1,5 @@
+@echo off
+title iCu main activator
+cd C:\iCures\Dependencies\libimdevice
+ideviceactivation activate -d -s localshost/sliver.php
+pause>nul
---
+++ iCures/runapache.bat
@@ -0,0 +1,7 @@
+@echo off
+title iCuApacheLauncher
+cd C:\iCures\Apache24\bin
+httpd
+
+echo ignore thr message above.
+pause>nul
