e88e1ec91186b70743bc075932bc1d6227aab731
DsSoft-Byte <69913272+DsSoft-Byte@users.noreply.github.com>
2022-06-09 09:05:30 +0200

    fix localhost to 127.0.0.1

--- iCures/idact.bat
+++ iCures/idact.bat
@@ -1,5 +1,5 @@
 @echo off
 title iCu main activator
 cd C:\iCures\Dependencies\libimdevice
-ideviceactivation activate -d -s localhost/sliver.php
+ideviceactivation activate -d -s 127.0.0.1/sliver.php
 pause>nul
